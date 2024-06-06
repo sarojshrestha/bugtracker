48d8c3b038146ae4d034fb5c322bf78c846913ed
mintokyo <sarojshresthaa@yahoo.com>
2024-06-06 16:48:19 +0900

    Build NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+import { FaBug } from "react-icons/fa";
+const NavBar = () => {
+  return (
+   <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+    <Link href="/"><FaBug /></Link>
+  <ul className='flex space-x-6'>
+    <li><Link href="/">Dashboard</Link></li>
+    <li><Link href="/issue">Issues</Link></li>
+  </ul>
+   </nav>
+  )
+}
+
+export default NavBar
